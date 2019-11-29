a5c4e29dc865a788fe5c11af8fd84553db7db675
sridharkritha <sridharkritha@gmail.com>
2019-11-29 07:16:00 +0000

    C++ changes

--- C++/C++11/examples/examples/main.cpp
+++ C++/C++11/examples/examples/main.cpp
@@ -1,9 +1,64 @@
-#if 1
 
 
 
 
+#if 0
 
+
+
+/* Distribution */
+
+int main ()  {
+	// engine only provides a source of randomness
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine e(seed);
+   // How to get a random number between 0 and 5?
+   //  e()%6  
+	//    -- Bad quality of randomness
+	//    -- Can only provide uniform distribution
+
+	std::uniform_int_distribution<int> distr(0,5);  // range: [0,5]  -- both 1 and 5 are included
+													// default param: [0, INT_MAX]
+	cout << " int_distribution: " << endl; 
+    for (int i=0; i<30; i++) {
+        cout << distr(e) << " ";
+    }
+
+
+	cout << "\n\n real_distribution: " << endl;
+
+	std::uniform_real_distribution<double> distrReal(0,5);  // half open: [1, 5)  -- 1 is included, 5 is not.
+														// default param: [0, 1)
+    for (int i=0; i<30; i++) {
+        cout << distrReal(e) << " ";
+    }
+
+	cout << " poisson_distribution: " << endl; 
+	std::poisson_distribution<int> distrP(1.0);  //  mean (double) 
+    for (int i=0; i<30; i++) {
+        cout << distrP(e) << " ";
+    }
+	cout << endl;	
+
+	cout << " normal_distribution: " << endl; 
+	std::normal_distribution<double> distrN(10.0, 3.0);  // mean and standard deviation
+	vector<int> v(20);
+    for (int i=0; i<800; i++) {
+        int num = distrN(e); // convert double to int
+		if (num >= 0 && num < 20)
+			v[num]++;   // E.g., v[10] records number of times 10 appeared
+    }
+	for (int i=0; i<20; i++) {
+		cout << i << ": " << std::string(v[i], '*') << endl;
+	}
+	cout << endl;
+
+	// Stop using rand()%n; 
+}
+
+/* Other distributions */
+
+#endif
 
 
 
@@ -37,6 +92,190 @@ int main()
 
 #else
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <chrono>
+#include <algorithm> // std::shuffle
+using namespace std;
+
+#if 0
+/* Random Engine:
+ *    stateful generator that generates random value within predefined min and max.
+ *    Not truely random -- pseudorandom
+ */
+int main ()
+{
+	std::default_random_engine eng;
+	cout << "Min: " << eng.min() << endl; // 0
+	cout << "Max: " << eng.max() << endl; // 4294967295
+
+	cout << eng() << endl;  // 3499211612; eng() - Functor which generate one random value
+	cout << eng() << endl;  // 581869302;  Generate a second random different value
+
+	std::stringstream state;
+	state << eng;  // Save the current internal state of the random engine
+	cout << eng() << endl;  // 3890346734; Generate one random value
+	cout << eng() << endl;  // 3586334585; Generate second random value
+
+	state >> eng;  // Restore the state
+	// It generates the same set of values in the same order
+	cout << eng() << endl;  // 3890346734; Generate one random value
+	cout << eng() << endl;  // 3586334585; Generate second random value
+}
+
+#else
+
+/* More examples */
+void printRandom(std::default_random_engine e) {
+	for (int i=0; i<10; i++) 
+		cout << e() << " ";
+	cout << endl;
+}
+
+template <typename T>
+void printArray(T arr) {
+	for (auto v:arr) {
+		cout << v << " ";
+	}
+	cout << endl;
+}
+
+int main ()
+{
+	std::default_random_engine eng1;
+	printRandom(eng1);
+
+	std::default_random_engine eng2;
+	printRandom(eng2); // eng1 = eng2
+
+	unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+	std::default_random_engine eng3(seed); // seed determines the internal state of an engine
+	printRandom(eng3); // eng3 is different from eng1 and eng 2
+
+	eng1.seed();  // reset engine to initial state
+	eng1.seed(109); // set engine to a state according to seed 109
+
+	eng2.seed(109);
+	if (eng1 == eng2)   // will return true
+		cout << "eng and eng2 have the same state" << endl;
+
+	// C++ 98
+	cout << "\n\n Shuffling:" << endl;
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+	vector<int> vec(arr, arr+9);
+	std::random_shuffle(vec.begin(), vec.end());
+	printArray(vec); // some random order
+
+	// C++ 11
+	vector<int> d =  {1,2,3,4,5,6,7,8,9};
+	std::shuffle(d.begin(), d.end(), std::default_random_engine());
+	printArray(d); // some random order
+	std::shuffle(d.begin(), d.end(), std::default_random_engine()); 
+	printArray(d); // totally some different random order
+	
+	std::shuffle(d.begin(), d.end(), eng1);
+	printArray(d); // some random order
+	std::shuffle(d.begin(), d.end(), eng1);
+	printArray(d); // totally some random order
+
+	auto rng = std::default_random_engine{};
+	std::shuffle(d.begin(), d.end(), rng);
+	printArray(d); // some random order
+	std::shuffle(d.begin(), d.end(), rng);
+	printArray(d); // totally some random order
+
+	std::shuffle(d.begin(), d.end(), std::default_random_engine{});
+	printArray(d); // some random order
+	std::shuffle(d.begin(), d.end(), std::default_random_engine{});
+	printArray(d); // totally some random order
+}
+
+/* Other random engines */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <chrono> // A precision-neutral(sec, millsec, micro, nano, pico etc) library for time and date
+using namespace std;
+int main() {
+
+	/* 
+	* CLOCKS:
+	*
+	* std::chrono::system_clock:  current time according to the system(toolbar clock) (it is not steady)
+	* std::chrono::steady_clock:  goes at a uniform rate (it can't be adjusted)
+	* std::chrono::high_resolution_clock: provides smallest possible tick period. 
+	*                   (might be a typedef of steady_clock or system_clock)
+	*
+	* clock period is represented with std:ratio<>
+	*/
+	std::ratio<2,10>  r; // 1/5
+	cout << r.num << "/" << r.den << endl; // 1/5
+	// period of my laptop system clock - 1/100,00000 == 100 nanoseconds
+	cout << chrono::system_clock::period::num << "/" << chrono::system_clock::period::den << endl; // 1/100,00000 == 100 nanoseconds
+	cout << chrono::steady_clock::period::num << "/" << chrono::steady_clock::period::den << endl; // 1/100,0000000
+	cout << chrono::high_resolution_clock::period::num << "/" << chrono::high_resolution_clock::period::den << endl; // 1/100,0000000
+
+
+	/*
+	* DURATION:
+	* std:chrono::duration<>:  represents time duration (ex: 2 hours, 30 minutes, 45 seconds)
+	*    duration<int, ratio<1,1>> --  number of seconds stored in a int  (this is the default)
+	*    duration<double, ration<60,1>> -- number of minutes (60 seconds) stored in a double
+	*    convenince duration typedefs in the library:
+	*    nanoseconds, microseconds, milliseconds, seconds, minutes, hours
+	* system_clock::duration  -- duration<T, system_clock::period>
+	*                                 T is a signed arithmetic type, could be int or long or others
+	*/
+	chrono::microseconds mi(2745); // 2745 microseconds
+	cout << mi.count() << std::endl;
+	chrono::nanoseconds na = mi;   
+	cout << na.count() << std::endl; // 2745000 nanoseconds
+	// when information loss could happen, convert explicitly (Truncated instead of rounding)
+	chrono::milliseconds mill = chrono::duration_cast<chrono::milliseconds>(mi); // 2 milliseconds
+	cout << mill.count() << std::endl; // 2
+	// No need for cast
+	mi = mill + mi;  // 2000 + 2745 = 4745
+	mill = chrono::duration_cast<chrono::milliseconds>(mill + mi);  // 6	
+	cout << mill.count() << std::endl;
+
+	cout << "min: " << chrono::system_clock::duration::min().count() << "\n"; // -9223372036854775808
+	cout << "max: " << chrono::system_clock::duration::max().count() << "\n"; // 9223372036854775807
+
+
+	/* 
+	* TIME POINT:
+	* std::chrono::time_point<>: represents a point of time
+	*       -- Length of time elapsed since a spacific time in history: 
+	*          00:00 January 1, 1970 (Corordinated Universal Time - UTC)  -- epoch of a clock
+	* time_point<system_clock, milliseconds>:  according to system_clock, the elapsed time since epoch in milliseconds
+	*
+	* typdefs
+	system_clock::time_point  -- time_point<system_clock, system_clock::duration>
+	steady_clock::time_point  -- time_point<steady_clock, steady_clock::duration>
+	*/
+	// Use system_clock
+	chrono::system_clock::time_point tp = chrono::system_clock::now();
+	// No. clock cycle elapsed since epoch
+	cout << tp.time_since_epoch().count() << endl; // 15750086245600500 count => 15750086245600500 x 100 nanoseconds
+	// Bcos my system clock period is -  1/100,00000 == 100 nanoseconds
+
+	tp = tp + chrono::seconds(2);  // no need for cast because tp is very high resolution
+	cout << tp.time_since_epoch().count() << endl;
+
+	// Better use steady_clock - for Calculate time interval
+	chrono::steady_clock::time_point start = chrono::steady_clock::now();
+	cout << "I am bored" << endl;
+	chrono::steady_clock::time_point end = chrono::steady_clock::now();
+	chrono::steady_clock::duration d = end - start;
+	if (d == chrono::steady_clock::duration::zero()) // incorrect d == 0, d is a duration, 0 is an integer
+		cout << "no time elapsed" << endl;
+	cout << chrono::duration_cast<chrono::microseconds>(d).count() << endl;
+	// Using system_clock may result in incorrect value
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* 
 Regular Expression:  pattern "match" strings of text.
 The C++ standard library supports multiple regular expression grammars.
