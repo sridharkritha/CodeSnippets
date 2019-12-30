96f6c2553c3c6fd09bd4be646260dbca217fbeba
sridharkritha <sridharkritha@gmail.com>
2019-12-30 18:55:59 +0000

    Update

---
+++ C++/C++11/examples/examples/Singleton.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////  Singleton.cpp  ////////////////////////
+#include "Singleton.h"
+#include "Dog.h"
+#include "Cat.h"
+
+Dog* Singleton::pd = 0;
+Cat* Singleton::pc = 0;
+
+Dog* Singleton::getDog() {
+	if(!pd) pd = new Dog("Gunner"); // Initialize Upon First Usage Idion
+	return pd;
+}
+
+Cat* Singleton::getCat() {
+	if(!pc) pc = new Cat("Smokey");
+	return pc;
+}
+
+Singleton::~Singleton() {
+	if(pd) delete pd;
+	if(pc) delete pc;
+	pd = 0; pc = 0;
+}
